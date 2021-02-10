3ee69e379140a17bc96cce71dff6d71e09550d08
IdoGo15 <idogolan15@gmail.com>
2021-02-10 12:31:16 +0200

    Updated Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import Users from '../users/Users';
 
 const Home = () => (
   <Fragment>
-    <h1 className="text-center large">Github Profile Finder</h1>
+    <h1 className="text-center large">GitHub Profile Finder</h1>
     <p className="text-center">Get the latest information about the user you are looking for
     <span> </span>
     <i class="fas fa-check"></i>
