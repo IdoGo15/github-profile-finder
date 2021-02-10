7f169a82a01462401f7a4b03e806d6c3e2acd80c
IdoGo15 <idogolan15@gmail.com>
2021-02-10 12:27:44 +0200

    Updated About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,9 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
+      <h1>About GitHub Profile Finder</h1>
+      <p>Search app for getting the latest information about the GitHub user you are looking for.</p>
+      <p>Developed by: Ido Golan</p>
       <p>Version: 1.0.0</p>
     </Fragment>
   );
